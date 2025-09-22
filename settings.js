7358d8ed1851884790ce525dcfa55b591432728c
IRONMAN x VENOM <gautamdipanshu475@gmail.com>
2025-09-22 13:30:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'ROBOT',
+  author: 'IRONMAN‎',
+  botName: "ROBOT",
+  botOwner: 'IRONM4N', // Your name
+  ownerNumber: '919719313814', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
